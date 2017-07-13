800b17838dace5bc0b50a294c9d1cf60524ab76b
kaserole <kaserole@24dcaea7d349>
2017-07-13 17:29:49 +0000

    pls work

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w68r0#!87fbyi25pm-m+&nrh3*1m-kzwl$&24mw*-8s=n0br9d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'kaserole.pythonanywhere.com']
 
 
 # Application definition
